4926eddc9867b5d3866b6b917126e3a0b958cad1
Joon Lee <joon@act.md>
2016-11-02 08:50:52 -0400

    Initial commit

---
+++ lib/normalizr/rails.rb
@@ -0,0 +1,7 @@
+require "normalizr/rails/version"
+
+module Normalizr
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/normalizr/rails/version.rb
@@ -0,0 +1,5 @@
+module Normalizr
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
